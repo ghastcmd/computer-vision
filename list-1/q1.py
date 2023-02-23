8ea0d5e80a75a479fedf1db74312c0d7b94acfff
Renalvo Alves <renalvojunior1@gmail.com>
2023-02-22 23:13:53 -0300

    feat: added code resolution

--- list-1/q1.py
+++ list-1/q1.py
@@ -1,1 +1,36 @@
-# here includes code for the question 1 of the list
+from PIL import Image
+import numpy as np
+
+def apply_filter(image, filter_size, filter_weights):
+    # convertendo a imagem em um numpy array
+    image = np.array(image)
+
+    # adicionando padding na imagem
+    padded_image = np.pad(image, filter_size//2, mode='constant')
+
+    # convertendo os pesos em uma matriz
+    filter_matrix = np.reshape(filter_weights, (filter_size, filter_size))
+
+    # fazendo a convolução
+    filtered_image = np.zeros(image.shape)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            window = padded_image[i:i+filter_size, j:j+filter_size]
+            filtered_image[i, j] = np.sum(window * filter_matrix)
+
+    # normalizando a imagem
+    filtered_image = (filtered_image - np.min(filtered_image)) / (np.max(filtered_image) - np.min(filtered_image))
+
+    return filtered_image
+
+
+image = Image.open('q1.ppm').convert('L')
+
+# definindo o tamanho do filtro e os pesos
+filter_size = 3
+filter_weights = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+# aplicando o filtro
+filtered_image = apply_filter(image, filter_size, filter_weights)
+
+Image.fromarray(np.uint8(filtered_image * 255)).save('../images/filtered_q1.ppm')
