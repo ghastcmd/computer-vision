83fcb3138471aa7961a4b47633edb26225b3111e
GhastCMD <kraken32bin@protonmail.com>
2023-02-23 23:48:41 -0300

    fix: high-pass and low-pass fix

--- list-1/q3.py
+++ list-1/q3.py
@@ -8,9 +8,9 @@ from PIL import Image
 
 from q1 import apply_filter
 
-images_files = ['./images/q2_1.ppm', './images/q2_1.ppm', './images/q2_1.ppm']
+images_files = ['./images/q2_1.ppm', './images/q2_2.ppm', './images/q2_3.ppm']
 
-images = [Image.open(f'{file}').convert('L') for file in images_files]
+images = [np.array(Image.open(f'{file}').convert('L'), dtype=np.uint8) for file in images_files]
 
 filter_sizes = [3, 6, 9]
 
@@ -45,7 +45,10 @@ kernels = list([
 images_conv = []
 for image in images:
     for size, kernel in zip(filter_sizes, kernels):
+        image = image / 255
         image_conv = apply_filter(image, size, kernel.flatten())
+        image_conv = image_conv / np.max(image_conv)
+
         images_conv.append(image_conv)
 
 images_conv_grouped = [images_conv[i:i+3] for i in range(0, len(images_conv), 3)]
@@ -56,5 +59,6 @@ for i, images in zip(range(1, 4), images_conv_grouped):
     for kernel_size, image in zip(kernel_sizes, images):
         Image.fromarray(np.uint8(image * 255)).save(f'{out_name}_{i}_{kernel_size}.ppm')
 
-# É perceptível que quanto maior o filtro de passa-alta, mais forte fica o efeito,
-# fazendo com que a imagem fique mais com as linhas da imagem mais acentuada.
+# É perceptível que quanto maior o filtro de passa-alta, mais grosso ficam as arestas,
+# fazendo com que a imagem fique mais com as linhas da imagem mais acentuada, e mais
+# perceptível o formato da imagem, de modo que pareça mais com a imagem original.
--- list-1/q4.py
+++ list-1/q4.py
@@ -8,9 +8,9 @@ from PIL import Image
 
 from q1 import apply_filter
 
-images_files = ['./images/q2_1.ppm', './images/q2_1.ppm', './images/q2_1.ppm']
+images_files = ['./images/q2_1.ppm', './images/q2_2.ppm', './images/q2_3.ppm']
 
-images = [Image.open(f'{file}').convert('L') for file in images_files]
+images = [np.array(Image.open(f'{file}').convert('L'), dtype=np.uint8) for file in images_files]
 
 filter_sizes = [3, 6, 9]
 
@@ -45,7 +45,10 @@ kernels = list([
 images_conv = []
 for image in images:
     for size, kernel in zip(filter_sizes, kernels):
+        image = image / 255
         image_conv = apply_filter(image, size, kernel.flatten())
+        image_conv = image_conv / np.max(image_conv)
+
         images_conv.append(image_conv)
 
 images_conv_grouped = [images_conv[i:i+3] for i in range(0, len(images_conv), 3)]
@@ -56,8 +59,7 @@ for i, images in zip(range(1, 4), images_conv_grouped):
     for kernel_size, image in zip(kernel_sizes, images):
         Image.fromarray(np.uint8(image * 255)).save(f'{out_name}_{i}_{kernel_size}.ppm')
 
-# O efeito de aumentar o tamanho do kernel do filtro passa-baixa é similar ao do
-# passa-alta, porém, ao contrário do passa-baixa que acentua as "arestas" da imagem,
-# o passa-alta aumenta a itensidade de tudo que não é aresta, porém, o efeito fica
-# visualmente bem parecido com o passa-baixa, pois ele aumenta a diferença entre as
-# arestas e o resto da imagem.
+# O efeito do tamanho do kernel no passa-alta é parecido com o do passa-baixa, mas que
+# é perceptível que em kernels menores a imagem ainda é bem fácil de ser reconhecida, 
+# porém, foi percebido que o passa-alta preenche mais com aresta o interior da imagem,
+# do que as arestas da mesma.
