9dea6c261c6133151d36bc4bea64dc27a8e6079c
GhastCMD <kraken32bin@protonmail.com>
2023-03-20 22:44:44 -0300

    feat: created compute sift descriptors function

--- list-2/q4.py
+++ list-2/q4.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 import skimage.draw
 import cv2
 
-def get_harris_corners_coords(image: np.ndarray, k = 0.05, threshold_corner = 0.001) -> list[tuple[int, int]]:
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+def get_harris_corners_coords(gray_image: np.ndarray, k = 0.05, threshold_corner = 0.001) -> list[tuple[int, int]]:
     Ix = cv2.Sobel(gray_image, cv2.CV_64F, dx=1, dy=0)
     Iy = cv2.Sobel(gray_image, cv2.CV_64F, dx=0, dy=1)
     
@@ -76,6 +74,47 @@ def draw_all_rectangles(image, coords):
         except:
             pass
 
+def compute_sift_descriptor(gray_image, coords, num_bins=8, bin_size=4):
+    ret_coords = []
+    ret_descriptors = []
+    
+    max_height = gray_image.shape[0]
+    max_width = gray_image.shape[1]
+
+    for x, y in coords:
+        if x < 8 or x > max_width - 8:
+            pass
+        if y < 8 or y > max_height - 8:
+            pass
+    
+        patch = gray_image[x-8:x+8, y-8:y+8]
+        
+        dx = cv2.Sobel(patch, ddepth=cv2.CV_64F, dx=1, dy=0)
+        dy = cv2.Sobel(patch, ddepth=cv2.CV_64F, dx=0, dy=1)
+
+        orientation = np.arctan2(dy, dx) * 180 / np.pi
+
+        sub_blocks = [(i, j) for i in range(0, 16, bin_size) for j in range(0, 16, bin_size)]
+
+        descriptor = np.zeros((num_bins * bin_size ** 2,))
+
+        for x, y in sub_blocks:
+            histogram = np.zeros((num_bins,))
+            for i in range(bin_size):
+                for j in range(bin_size):
+                    bin_index = np.int32(orientation[x+i, y+j] / (360 / num_bins))
+                    histogram[bin_index] += 1
+            descriptor[x + (y * bin_size):x + y * bin_size + num_bins] = histogram[:]
+        
+        norm = np.linalg.norm(descriptor)
+        if norm > 0:
+            descriptor /= norm
+        
+        ret_coords.append((x, y))
+        ret_descriptors.append(descriptor)
+    
+    return np.array(ret_coords), np.array(ret_descriptors)
+    
 if __name__ == '__main__':
     root_path = './images'
     image_filepath_1 = 'calculator-1.jpg'
@@ -86,13 +125,21 @@ if __name__ == '__main__':
     image_1 = cv2.resize(image_1, resize_tuple_scaled(image_1.shape, 8))
     image_2 = cv2.resize(image_2, resize_tuple_scaled(image_2.shape, 8))
     
+    # getting gray images
+    gray_image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2GRAY)
+    gray_image_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2GRAY)
+    
     # getting corner's coordinates
-    corner_coords_image_1 = get_harris_corners_coords(image_1)
-    corner_coords_image_2 = get_harris_corners_coords(image_2)
+    corner_coords_image_1 = get_harris_corners_coords(gray_image_1)
+    corner_coords_image_2 = get_harris_corners_coords(gray_image_2)
 
     # converting images to print on matplotlib
     image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2RGB)
     image_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2RGB)
+
+    # getting sift descriptors for each point gotten with harris corner detection
+    coords_image_1, descriptors_image_1 = compute_sift_descriptor(gray_image_1, corner_coords_image_1)
+    coords_image_2, descriptors_image_2 = compute_sift_descriptor(gray_image_2, corner_coords_image_1)
 
     # drawing rectangles for each corner centroid
     to_draw_image_1 = np.copy(image_1)
